f30ae1ef95ad9188ccd0423f2828d1fcfc909f27
Dro0804 <getuyazandrouriel@gmail.com>
2025-10-08 14:11:36 +0930

    d
    d

--- livingarchive/urls.py
+++ livingarchive/urls.py
@@ -4,11 +4,17 @@ from django.urls import include, path
 from wagtail import urls as wagtail_urls
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.documents import urls as wagtaildocs_urls
+from django.urls import path
+from . import views
 
 from search import views as search_views
 from user_group_management.views import GroupProfileListView, GroupMembersView, RemoveMemberView
 
 urlpatterns = [
+    path("3d/", views.cesium_view, name="cesium_view"),
+    path("api/annotations.geojson", views.annotations_geojson),
+    path("api/annotations/", views.annotations_post),
+    
     path("", include("home.urls")),   # homepage
 
     path("django-admin/", admin.site.urls),
